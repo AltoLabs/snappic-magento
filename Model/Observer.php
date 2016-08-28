4df2f366a5b427b80bfa9267e9aa9d61f7bbc85e
MARTIN Pierre <HicksCorp@GMail.com>
2016-08-28 12:25:50 +0100

    Correctly sends product and images to the API.

--- Model/Observer.php
+++ Model/Observer.php
@@ -4,24 +4,29 @@
 */
 
 const SHARED_SECRET = 'abc123456';
+const SNAPPIC_HOST = 'http://dockerhost:3000';
+// const SNAPPIC_HOST = 'https://api.snappic.io';
 
 class Altolabs_Snappic_Model_Observer {
   public function onAfterOrderPlace(Varien_Event_Observer $observer) {
+    Mage::Log('-------------------->>> onAfterOrderPlace');
     $order = $observer->getEvent()->getOrder();
-    $sendable = [self::sendableOrderData($order)];
+    $sendable = self::sendableOrderData($order);
     self::notifySnappicApi('orders/paid', $sendable);
   }
 
   public function onProductAfterSave(Varien_Event_Observer $observer) {
+    Mage::Log('-------------------->>> onProductAfterSave');
     $product = $observer->getEvent()->getProduct();
     if ($product->hasDataChanges()) {
-      $sendable = [self::sendableProductData($product)];
-      self::notifySnappicApi('products/update', $sendable);
+      $sendable = self::sendableProductData($product);
+      self::notifySnappicApi('products/update', [$sendable]);
     }
     return $this;
   }
 
   public function onProductAfterAttributeUpdate(Varien_Event_Observer $observer) {
+    Mage::Log('-------------------->>> onProductAfterAttributeUpdate');
     $productIds = $observer->getEvent()->getProductIds();
     $productModel = Mage::getModel('catalog/product');
     $sendable = array();
@@ -35,8 +40,9 @@ class Altolabs_Snappic_Model_Observer {
   }
 
   public function onProductAfterDelete(Varien_Event_Observer $observer) {
+    Mage::Log('-------------------->>> onProductAfterDelete');
     $product = $observer->getEvent()->getProduct();
-    $sendable = [$product->getId()];
+    $sendable = $product->getId();
     $this->notifySnappicApi('products/delete', $sendable);
     return $this;
   }
@@ -45,17 +51,17 @@ class Altolabs_Snappic_Model_Observer {
    * @param $topic string is the type of event to be sent.
    * @param $sendable object is the actual payload to be serialized and sent. */
   private function notifySnappicApi($topic, $sendable) {
-    Mage::Log(self::getStoreDomain());
-    $sendable = json_encode($sendable);
-    $client = new Zend_Http_Client('https://api.snappic.io/webhooks/magento/product');
+    $client = new Zend_Http_Client(SNAPPIC_HOST . '/magento/webhooks');
     $client->setMethod(Zend_Http_Client::POST);
+    //$client->setParameterPost('data', $sendable);
+    $sendable = json_encode(['data' => $sendable ]);
+    $client->setRawData($sendable);
     $client->setHeaders([
       'Content-type'                => 'application/json',
-      'X-Magento-Shop-Domain'       => self::getStoreDomain(),
+      'X-Magento-Shop-Domain'       => Mage::getBaseUrl(),
       'X-Magento-Topic'             => $topic,
       'X-Magento-Webhook-Signature' => self::signPayload($sendable)
     ]);
-    $client->setParameterPost('data', $sendable);
     try{
       $response = $client->request();
       if (!$response->isSuccessful()) return false;
@@ -65,23 +71,53 @@ class Altolabs_Snappic_Model_Observer {
     return true;
   }
 
-  static private function getStoreDomain() {
-    $baseUrl = Mage::getBaseUrl();
-    return substr($baseUrl, strpos($baseUrl, '://') + 3);
-  }
-
   static private function sendableProductData(Mage_Catalog_Model_Product $product) {
     return [
+      'id'              => $product->getId(),
       'title'           => $product->getName(),
       'description'     => $product->getDescription(),
       'handle'          => $product->getUrlKey(),
       'updated_at'      => $product->getUpdatedAt(),
-      'image_url'       => $product->getImageUrl()
+      'images'          => self::sendableImagesData($product->getMediaGalleryImages()),
+      'options'         => self::sendableOptionsData($product->getOptions())
+    ];
+  }
+
+  static private function sendableImagesData(Varien_Data_Collection $images) {
+    $sendable = [];
+    foreach ($images as $image) {
+      $sendable[] = self::sendableImageData($image);
+    }
+    return $sendable;
+  }
+  static private function sendableImageData($image) {
+    return [
+      'id'            => $image->getId(),
+      'src'           => $image->getUrl(),
+      'position'      => $image->getPosition(),
+      'updated_at'    => $image->getUpdatedAt()
+    ];
+  }
+
+  static private function sendableOptionsData(Varien_Data_Collection $options) {
+    $sendable = [];
+    foreach ($options as $option) {
+      $sendable[] = self::sendableImageData($option);
+    }
+    return $sendable;
+  }
+  static private function sendableOptionData($option) {
+    return [
+      'id'            => $option->getId(),
+      'name'          => $option->name(),
+      'position'      => $option->getPosition(),
+      'values'        => $option->getValues()
     ];
   }
 
   static private function sendableOrderData(Mage_Sales_Model_Order $order) {
     return [
+      'id'                      => $order->getId(),
       'number'                  => $order->getId(),
       'order_number'            => $order->getId(),
       'email'                   => $order->getEmail(),
