1a2ae9c804cac35af2e80b6f746ed04ef6a2b57c
Robbie Averill <robbie.averill@kathmandu.co.nz>
2016-09-09 06:46:53 +1200

    Add store data API models and configuration
    * Rename etc/modules/Altolabs_Snappic.xml to AltoLabs...
    * Add REST API configuration and store API model data

---
+++ Model/Api2/Snappic/Store.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Provide information about the current store
+ *
+ * @category Mage
+ * @package  AltoLabs_Snappic
+ * @author   AltoLabs <hi@altolabs.co>
+ */
+class AltoLabs_Snappic_Model_Api2_Snappic_Store extends Mage_Api2_Model_Resource
+{
+
+}
---
+++ Model/Api2/Snappic/Store/Rest/Admin/V1.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Provide information about the current store
+ *
+ * @category Mage
+ * @package  AltoLabs_Snappic
+ * @author   AltoLabs <hi@altolabs.co>
+ */
+class AltoLabs_Snappic_Model_Api2_Snappic_Store_Rest_Admin_V1 extends AltoLabs_Snappic_Model_Api2_Snappic_Store
+{
+    /**
+     * Get information about the current store
+     * @return array
+     */
+    protected function _retrieve()
+    {
+        /** @var Mage_Core_Model_Store $store */
+        $store = Mage::app()->getDefaultStoreView();
+
+        // Add custom paramaters
+        $store
+            ->setName($store->getName())
+            ->setStoreGroupName($store->getGroup()->getName())
+            ->setStoreDomain($store->getBaseUrl())
+            ->setIanaTimezone(Mage::getStoreConfig(Mage_Core_Model_Locale::XML_PATH_DEFAULT_TIMEZONE, $store->getId()))
+            ->setBaseCurrency($store->getBaseCurrencyCode())
+            ->setMoneyWithCurrencyFormat(Mage::helper('core')->currency(10.00, true, false));//'$ {{10.00}} ' . $store->getBaseCurrencyCode());
+
+        return (array) $store->getData();
+    }
+}
