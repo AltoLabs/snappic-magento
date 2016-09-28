2f8e9d4b88fc7cae1ba0c064500b072efb46284a
MARTIN Pierre <HicksCorp@GMail.com>
2016-09-28 21:17:40 +0100

    Fixes install script. Adds configurable variables.
    Fixes install script. Adds configurable variables.

--- app/code/local/AltoLabs/Snappic/sql/snappic_setup/mysql4-install-0.0.1.php
+++ app/code/local/AltoLabs/Snappic/sql/snappic_setup/mysql4-install-0.0.1.php
@@ -2,14 +2,14 @@
 
 $endl = "\r\n";
 
-echo("Performing AltoLabs Snappic Extension installation...$endl");
+Mage::Log("Performing AltoLabs Snappic Extension installation...$endl");
 
 $oauthHelper = Mage::helper('oauth');
 
-echo("Checking for the Snappic user...$endl");
+Mage::Log("Checking for the Snappic user...$endl");
 $user = Mage::getModel('admin/user')->load('snappic', 'username');
 if (!$user->getId()) {
-  echo("User was not found, creating...$endl");
+  Mage::Log("User was not found, creating...$endl");
   $user = Mage::getModel('admin/user')
       ->setData(array(
           'username'  => 'snappic',
@@ -21,21 +21,21 @@ if (!$user->getId()) {
       ->save();
 }
 
-echo("Checking for the Snappic Role...$endl");
+Mage::Log("Checking for the Snappic Role...$endl");
 $adminRole = Mage::getModel('api2/acl_global_role')->load('Snappic', 'role_name');
 if (!$adminRole->getId()) {
-    echo("Role was not found, creating...$endl");
+    Mage::Log("Role was not found, creating...$endl");
     $adminRole = Mage::getModel('api2/acl_global_role')
         ->setData(array('role_name' => 'Snappic'))
         ->save();
 }
 
-echo("Configuring ACLs...$endl");
+Mage::Log("Configuring ACLs...$endl");
 $adminRoleId = $adminRole->getId();
 foreach (['snappic_product', 'snappic_store'] as $snappicResource) {
     $globalRule = Mage::getModel('api2/acl_global_rule')->load($snappicResource, 'resource_id');
     if ($globalRule->getId()) { continue; }
-    echo("Allowing the Snappic Role to retrieve $snappicResource...$endl");
+    Mage::Log("Allowing the Snappic Role to retrieve $snappicResource...$endl");
     Mage::getModel('api2/acl_global_rule')
         ->setRoleId($adminRoleId)
         ->setResourceId($snappicResource)
@@ -48,7 +48,7 @@ $aclsByResource = array(
   'snappic_product' => 'id,title,description,price,handle,updated_at,variants,images,options'
 );
 foreach ($aclsByResource as $resource => $attributes) {
-  echo("Checking for ACLs for $resource...$endl");
+  Mage::Log("Checking for ACLs for $resource...$endl");
     $aclEntry = Mage::getModel('api2/acl_filter_attribute')
         ->getCollection()
         ->addFieldToFilter('user_type', Mage_Api2_Model_Acl_Global_Role::ROLE_CONFIG_NODE_NAME_ADMIN)
@@ -56,18 +56,17 @@ foreach ($aclsByResource as $resource => $attributes) {
         ->addFieldToFilter('operation', Mage_Api2_Model_Resource::OPERATION_ATTRIBUTE_READ)
         ->getFirstItem();
     if (!$aclEntry->getId()) {
-      echo("Creating ACLs for $snappicResource...$endl");
+      Mage::Log("Creating ACLs for $snappicResource...$endl");
       $aclEntry = Mage::getModel('api2/acl_filter_attribute')
                       ->setUserType(Mage_Api2_Model_Acl_Global_Role::ROLE_CONFIG_NODE_NAME_ADMIN)
                       ->setResourceId($resource)
                       ->setOperation(Mage_Api2_Model_Resource::OPERATION_ATTRIBUTE_READ);
     }
-    echo("Updating attributes for ACLs $snappicResource...$endl");
+    Mage::Log("Updating attributes for ACLs $snappicResource...$endl");
     $aclEntry->setAllowedAttributes($attributes)->save();
 }
 
-echo("Preparing the Snappic OAuth Consumer...$endl");
-echo(Mage::getModel('oauth/consumer'));
+Mage::Log("Preparing the Snappic OAuth Consumer...$endl");
 $consumer = Mage::getModel('oauth/consumer')->load('Snappic', 'name');
 if (!$consumer->getId()) {
     $consumer = Mage::getModel('oauth/consumer')
@@ -80,5 +79,10 @@ if (!$consumer->getId()) {
         ->save();
 }
 
-echo("AltoLabs Snappic Setup complete.$endl");
-echo('Key=' . $consumer->getData('key') . ', Secret=' . $consumer->getData('secret') . $endl);
+Mage::getStoreConfig('snappic/general/facebook_pixel_id')
+
+Mage::Log("AltoLabs Snappic Setup complete.$endl");
+Mage::Log('Key=' . $consumer->getData('key') . ', Secret=' . $consumer->getData('secret') . $endl);
+
+
+Mage::app()->getConfig()->saveConfig('snappic/general/facebook_pixel_id', $facebookID)
