2f0366e30b73c98d04c2108658e81434f24140a6
MARTIN Pierre <HicksCorp@GMail.com>
2016-10-09 22:42:44 +0100

    Style.

--- app/code/community/AltoLabs/Snappic/data/snappic_setup/data-upgrade-0.0.1-0.0.2.php
+++ app/code/community/AltoLabs/Snappic/data/snappic_setup/data-upgrade-0.0.1-0.0.2.php
@@ -14,12 +14,11 @@ $installer->startSetup();
 
 $installer->run("
     DROP TABLE IF EXISTS {$installer->getTable('altolabs_snappic/session')};
-
     CREATE TABLE {$installer->getTable('altolabs_snappic/session')} (
-    id int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
-    soap_session_id varchar(250) DEFAULT NULL,
-    quote_id varchar(250) DEFAULT NULL,
-    PRIMARY KEY (id)
+      id int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
+      soap_session_id varchar(250) DEFAULT NULL,
+      quote_id varchar(250) DEFAULT NULL,
+      PRIMARY KEY (id)
     ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
   ");
 
