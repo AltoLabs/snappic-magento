1c1c05ae3e8b5f8e31d8137d9aa5f46ed5024732
MARTIN Pierre <HicksCorp@GMail.com>
2016-10-10 17:31:17 +0100

    Comments on the new installation script.

--- app/code/community/AltoLabs/Snappic/data/snappic_setup/data-upgrade-0.0.2-0.0.3.php
+++ app/code/community/AltoLabs/Snappic/data/snappic_setup/data-upgrade-0.0.2-0.0.3.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This script takes care of storing a proper API key for the snappic frontend to use.
+ *
+ * This file is Copyright AltoLabs 2016.
+ *
+ * @category Mage
+ * @package  AltoLabs_Snappic
+ * @author   AltoLabs <hi@altolabs.co>
+ */
 
 Mage::log('Checking for SOAP user...', null, SNAPPIC_LOG);
 $apiKey = Mage::helper('altolabs_snappic')->getSoapApiKey();
