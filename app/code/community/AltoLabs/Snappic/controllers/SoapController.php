e9d07d9d5b7455303cf25879966b7b6ee280d4ae
MARTIN Pierre <HicksCorp@GMail.com>
2016-10-13 02:28:51 +0100

    Adds back soap controller.

---
+++ app/code/community/AltoLabs/Snappic/controllers/SoapController.php
@@ -0,0 +1,27 @@
+<?php
+class AltoLabs_Snappic_SoapController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $session = Mage::getSingleton('core/session');
+        $storeId = Mage::app()->getStore()->getStoreId();
+        $sessionId = $session->getSoapSessionId();
+        if (empty($sessionId)) {
+            try {
+                $username = 'Snappic';
+                $apiKey = Mage::helper('altolabs_snappic')->getSoapApiKey();
+                $wsdlUrl = Mage::getUrl('api.php') . '?type=v2_soap&wsdl=1';
+                $client = new SoapClient($wsdlUrl);
+                $sessionId = $client->login($username, $apiKey);
+                $session->setSoapSessionId($sessionId);
+            } catch (Exception $e) {
+                $message = $e->getMessage();
+                Mage::log("SOAP Exception: $message", NULL, 'snappic.log');
+                die($message);
+            }
+        }
+        $response = $this->getResponse();
+        $response->setHeader('Content-type', 'application/json');
+        $response->setBody(json_encode(array('storeId' => $storeId, 'sessionId' => $sessionId)));
+    }
+}
