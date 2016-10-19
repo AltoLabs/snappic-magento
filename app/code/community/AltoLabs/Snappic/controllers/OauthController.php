f3b08ca0536368df36a58e73caf9f31c2986b681
MARTIN Pierre <HicksCorp@GMail.com>
2016-10-19 23:11:59 +0100

    Fixes pin issues.

--- app/code/community/AltoLabs/Snappic/controllers/OauthController.php
+++ app/code/community/AltoLabs/Snappic/controllers/OauthController.php
@@ -14,12 +14,21 @@ class AltoLabs_Snappic_OauthController extends Mage_Core_Controller_Front_Action
       $this->loadLayout();
       $this->getLayout()->getBlock('head')->addJs('jsoauth.js');
       $block = $this->getLayout()->createBlock('core/text');
-      $block->setText($this->pageHtml());
+      $block->setText($this->indexHtml());
+      $this->getLayout()->getBlock('head')->append($block);
+      $this->renderLayout();
+    }
+
+    public function callbackAction()
+    {
+      $this->loadLayout();
+      $block = $this->getLayout()->createBlock('core/text');
+      $block->setText($this->callbackHtml());
       $this->getLayout()->getBlock('content')->append($block);
       $this->renderLayout();
     }
 
-    protected function pageHtml()
+    protected function indexHtml()
     {
         $helper = Mage::helper('altolabs_snappic');
         $domain = $helper->getDomain();
@@ -28,8 +37,7 @@ class AltoLabs_Snappic_OauthController extends Mage_Core_Controller_Front_Action
         $consumer = Mage::getModel('oauth/consumer')->load('Snappic', 'name');
         $consumerKey = $consumer->getKey();
         $consumerSecret = $this->getRequest()->getParam('secret');
-        $expectedConsumerSecret = $consumer->getSecret();
-        if ($consumerSecret != $expectedConsumerSecret) { return ''; }
+        if ($consumerSecret != $consumer->getSecret()) { return; }
         return "
           <script>
             var oauth = new OAuth({
@@ -37,33 +45,46 @@ class AltoLabs_Snappic_OauthController extends Mage_Core_Controller_Front_Action
               consumerSecret: '$consumerSecret',
               requestTokenUrl: 'http://$domain/oauth/initiate',
               authorizationUrl:  'http://$domain/$adminHtml/oauth_authorize',
-              accessTokenUrl: 'http://$domain/oauth/token'
+              accessTokenUrl: 'http://$domain/oauth/token',
+              callbackUrl: 'http://$domain/shopinsta/oauth/callback'
             });
-            oauth.fetchRequestToken(openAuthoriseWindow, failureHandler);
-            function openAuthoriseWindow(url) {
-              var wnd = window.open(url, 'authorise');
-              setTimeout(waitForPin, 100);
-              function waitForPin() {
-                if (wnd.closed) {
-                  var pin = prompt('Please enter your PIN', '');
-                  if (pin == null || pin == '') { return; }
-                  oauth.setVerifier(pin);
-                  oauth.fetchAccessToken(successHandler, failureHandler);
-                } else {
-                  setTimeout(waitForPin, 100);
-                }
-              }
+
+            this.setPin = function(pin) {
+              oauth.setVerifier(pin);
+              oauth.fetchAccessToken(successHandler, failureHandler);
             }
+
+            oauth.fetchRequestToken(openAuthoriseWindow, failureHandler);
+
+            function openAuthoriseWindow(url) {
+              window.open(url, 'authorise');
+            }
+
             function successHandler() {
-              var ret = document.createElement('div');
-              ret.setAttribute('id', 'store_access_token');
-              ret.setAttribute('hidden', '');
-              ret.innerHTML = '$consumerKey:$consumerSecret' + oauth.getAccessTokenKey() + ':' + oauth.getAccessTokenSecret();
-              document.body.append(ret);
+
+              // var elem = document.createElement('div');
+              // elem.setAttribute('id', 'store_access_token');
+              // elem.setAttribute('hidden', '');
+              // elem.innerHTML = '$consumerKey:$consumerSecret:' + oauth.getAccessTokenKey() + ':' + oauth.getAccessTokenSecret();
+              // document.body.append(elem);
+              console.log('Life is beautiful.');
             }
             function failureHandler(data) {
               console.error(data);
             }
+          </script>
+        ";
+    }
+
+    protected function callbackHtml()
+    {
+        return "
+          <script>
+            qs = document.location.search.split('+').join(' ');
+            var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g;
+            while (tokens = re.exec(qs)) { params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]); }
+            window.opener.setPin(params['oauth_verifier']);
+            window.close();
           </script>
         ";
     }
