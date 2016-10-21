9a3305ba327b3b5905a5ec3a9af9a4ac7bf7efa2
MARTIN Pierre <HicksCorp@GMail.com>
2016-10-21 12:51:53 +0100

    Various changes for submitting the extension, adds sample image directory to extension for Jason to put things in, removes obsolete migrations.

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo "Removing Snappic Magento Extension from local instance..."
+
 cd ../web/app
+
 rm -rf code/community/AltoLabs \
        etc/modules/AltoLabs_Snappic.xml \
        design/frontend/base/default/layout/snappic.xml \
