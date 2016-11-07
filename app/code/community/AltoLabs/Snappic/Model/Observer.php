89bcb8797adc50b02ca5e32342220b5c18bdadda
MARTIN Pierre <HicksCorp@GMail.com>
2016-11-07 23:15:52 +0000

    Fixes pixel cart bug.

--- app/code/community/AltoLabs/Snappic/Model/Observer.php
+++ app/code/community/AltoLabs/Snappic/Model/Observer.php
@@ -36,7 +36,18 @@ class Altolabs_Snappic_Model_Observer
      */
     public function onAfterProductAddToCart(Varien_Event_Observer $observer)
     {
-        Mage::getSingleton('core/session')->setCartProductJustAdded(true);
+        $product = Mage::getModel('catalog/product')->load(Mage::app()->getRequest()->getParam('product', 0));
+        if ($product->getId()) {
+          Mage::getSingleton('core/session')->setCartProductJustAdded(
+              new Varien_Object(array(
+                  'id' => $product->getId(),
+                  'qty' => Mage::app()->getRequest()->getParam('qty', 1),
+                  'name' => $product->getName(),
+                  'price' => $product->getPrice(),
+                  'category_name' => Mage::getModel('catalog/category')->load($categories[0])->getName(),
+              ))
+          );
+        }
         return $this;
     }
 
