8a5ba8ee1109cb111e9416c8eece4cf92e865cd0
MARTIN Pierre <HicksCorp@GMail.com>
2017-03-15 12:23:50 +0000

    Fixes add to cart configurable issue, packages 1.0.5.

--- app/code/community/AltoLabs/Snappic/controllers/CartController.php
+++ app/code/community/AltoLabs/Snappic/controllers/CartController.php
@@ -23,28 +23,55 @@ class AltoLabs_Snappic_CartController extends Mage_Core_Controller_Front_Action 
     $storeId = Mage::app()->getStore()->getId();
     $payload = Mage::helper('core')->jsonDecode($this->getRequest()->getRawBody());
     $product = Mage::getModel('catalog/product')->load($payload['id'])->setStoreId($storeId);
-    if ($product->getId()) {
-      try {
-        $cart->addProduct($product);
-        if (!$cart->getCustomerSession()->getCustomer()->getId() &&
-            $cart->getQuote()->getCustomerId()) {
-          $cart->getQuote()->setCustomerId(null);
-        }
-        $cart->save();
-        $this->_getSession()->setCartWasUpdated(true);
-        $this->_output(array(
-          'status' => 'success',
-          'total' => ($cart->getQuote()->getSubtotal() ?: '0.00')
-        ));
-      } catch (Exception $e) {
-        $this->_output(array(
-          'error' => $e->getMessage(),
-          'total' => ($cart->getQuote()->getSubtotal() ?: '0.00')
-        ));
-      }
-    } else {
+
+    if (!$product->getId()) {
+      Mage::log('Product with ID '.$payload['id'].' was not found.', null, 'snappic.log');
       $this->_output(array(
         'error' => 'The product was not found.',
+        'total' => ($cart->getQuote()->getSubtotal() ?: '0.00')
+      ));
+      return;
+    }
+
+    try {
+      # If product is part of configurables.
+      $parentIds = Mage::getModel('catalog/product_type_configurable')->getParentIdsByChild($product->getId());
+      if (count($parentIds) != 0) {
+        foreach ($parentIds as $parentId) {
+          $parent = Mage::getModel('catalog/product')->load($parentId);
+          $attrOpts = $parent->getTypeInstance(true)->getConfigurableAttributesAsArray($parent);
+          $attrs = array();
+          foreach ($attrOpts as $attr) {
+            $vals = array_column($attr['values'], 'value_index');
+            $curVal = $product->getData($attr['attribute_code']);
+            if (in_array($curVal, $vals)) {
+              $attrs[$attr['attribute_id']] = $curVal;
+            }
+          }
+          $req = new Varien_Object();
+          $req->setData(array('product' => $parentId, 'qty' => 1, 'super_attribute' => $attrs));
+          $cart->addProduct($parent, $req);
+          break;
+        }
+      }
+      # No parent ID just add the product.
+      else {
+        $cart->addProduct($product);
+      }
+
+      if (!$cart->getCustomerSession()->getCustomer()->getId() &&
+          $cart->getQuote()->getCustomerId()) {
+        $cart->getQuote()->setCustomerId(null);
+      }
+      $cart->save();
+      $this->_getSession()->setCartWasUpdated(true);
+      $this->_output(array(
+        'status' => 'success',
+        'total' => ($cart->getQuote()->getSubtotal() ?: '0.00')
+      ));
+    } catch (Exception $e) {
+      $this->_output(array(
+        'error' => $e->getMessage(),
         'total' => ($cart->getQuote()->getSubtotal() ?: '0.00')
       ));
     }
