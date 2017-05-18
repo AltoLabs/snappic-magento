378ffd5cd60ffebcbe2f98aa47e4fa5bcacc068c
MARTIN Pierre <HicksCorp@GMail.com>
2017-05-18 16:48:17 +0100

    Adds a sandboxed mode, bumps version and packages 1.0.9.

--- app/code/community/AltoLabs/Snappic/data/snappic_setup/data-install-1.0.0.php
+++ app/code/community/AltoLabs/Snappic/data/snappic_setup/data-install-1.0.0.php
@@ -3,4 +3,18 @@
 
 $installer = $this;
 $installer->startSetup();
+
+$helper = Mage::helper('altolabs_snappic');
+
+$token = $helper->getToken();
+$secret = $helper->getSecret();
+
+$connect = Mage::getSingleton('altolabs_snappic/connect');
+$connect->setSendable(array('token' => $token, 'secret' => $secret))
+        ->notifySnappicApi('application/installed');
+
+$configPath = $helper->getConfigPath('environment/sandboxed');
+Mage::app()->getConfig()->saveConfig($configPath, 1);
+Mage::app()->getConfig()->reinit();
+
 $installer->endSetup();
